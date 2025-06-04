0f4f830f15737c1de36b6b6eaa0f709bcef1d141
Clébio Júnior <clebiojuniorcj@gmail.com>
2025-06-04 13:57:15 -0300

    added summarization code

--- dstoolkit/nlp/__init__.py
+++ dstoolkit/nlp/__init__.py
@@ -1,5 +1,7 @@
 from .summarization.word_frequency import WordFreqSummarizer
+from .summarization.cosine_distance import CosineDistanceSummarizer
 
 __all__ = [
-    "WordFreqSummarizer"
+    "WordFreqSummarizer",
+    "CosineDistanceSummarizer"
 ]
