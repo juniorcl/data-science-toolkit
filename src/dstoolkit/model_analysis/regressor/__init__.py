e5177d8f219bd823472604ddb7af1de31c1a9d71
Júnior <clebiojuniorcj@gmail.com>
2025-12-31 16:43:38 -0300

    feat: add plot y_true vs y_pred

--- src/dstoolkit/model_analysis/regressor/__init__.py
+++ src/dstoolkit/model_analysis/regressor/__init__.py
@@ -1,8 +1,10 @@
 from .waste_distribution import plot_waste_distribution
 from .y_true_vs_y_pred import plot_pred_vs_true
+from .error_by_quantile import plot_error_by_quantile
 
 
 __all__ = [
     "plot_waste_distribution",
-    "plot_pred_vs_true"
+    "plot_pred_vs_true",
+    "plot_error_by_quantile"
 ]
