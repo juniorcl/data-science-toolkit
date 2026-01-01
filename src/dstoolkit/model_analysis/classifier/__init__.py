466ff3911d05021461ed5fcd78e9b87dd4ff6dd2
Júnior <clebiojuniorcj@gmail.com>
2026-01-01 19:42:51 -0300

    feat: add path for the functions

--- src/dstoolkit/model_analysis/classifier/__init__.py
+++ src/dstoolkit/model_analysis/classifier/__init__.py
@@ -1,6 +1,12 @@
 from .ks_curve import plot_ks_curve
+from .precision_recall_curve import plot_precision_recall_curve
+from .roc_curve import plot_roc_curve
+from .learning_curve import plot_learning_curve
 
 
 __all__ = [
-    "plot_ks_curve"
+    "plot_ks_curve",
+    "plot_precision_recall_curve",
+    "plot_roc_curve",
+    "plot_learning_curve",
 ]
