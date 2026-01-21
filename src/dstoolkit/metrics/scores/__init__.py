516d355817f8c7614f5ce9cef4abc9b610a4275d
Júnior <clebiojuniorcj@gmail.com>
2026-01-21 18:43:53 -0300

    refactor: refactor classifier metrics and scores

--- src/dstoolkit/metrics/scores/__init__.py
+++ src/dstoolkit/metrics/scores/__init__.py
@@ -1,12 +1,15 @@
-from .ks_score import ks_score
-from .ks_scorer import ks_scorer
 from .average_precision_lift_score import average_precision_lift_score
 from .average_precision_lift_scorer import average_precision_lift_scorer
-
+from .classifier_metrics import get_classifier_metrics
+from .classifier_score import get_classifier_score
+from .ks_score import ks_score
+from .ks_scorer import ks_scorer
 
 __all__ = [
-    'ks_score', 
-    'ks_scorer',
-    'average_precision_lift_score',
-    'average_precision_lift_scorer'
+    "ks_score",
+    "ks_scorer",
+    "average_precision_lift_score",
+    "average_precision_lift_scorer",
+    "get_classifier_metrics",
+    "get_classifier_score",
 ]
---
+++ src/dstoolkit/metrics/scores/classifier_metrics.py
@@ -0,0 +1,65 @@
+from sklearn.metrics import (
+    balanced_accuracy_score,
+    brier_score_loss,
+    f1_score,
+    log_loss,
+    precision_score,
+    recall_score,
+    roc_auc_score,
+)
+
+from .average_precision_lift_score import average_precision_lift_score
+from .ks_score import ks_score
+
+
+def get_classifier_metrics(y, target, pred_col="pred", prob_col="prob"):
+    """
+    Calculate a set of classification metrics.
+
+    This function computes various classification metrics based on the true and predicted values.
+
+    Parameters
+    ----------
+    y : pd.DataFrame
+        DataFrame containing the true labels and predictions.
+    pred_col : str
+        Name of the column with predicted class labels.
+    prob_col : str
+        Name of the column with predicted probabilities for the positive class.
+    target : str
+        Name of the column with true class labels.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the calculated metrics:
+        - 'Balanced Accuracy'
+        - 'Precision'
+        - 'Recall'
+        - 'F1'
+        - 'AUC'
+        - 'KS'
+        - 'Brier'
+        - 'LogLoss'
+        - 'Average Precision Lift'
+
+    Raises
+    ------
+    ValueError
+        If the input data is not valid for classification.
+    """
+    y_true = y[target]
+    y_pred = y[pred_col]
+    y_prob = y[prob_col]
+
+    return {
+        "Balanced Accuracy": balanced_accuracy_score(y_true, y_pred),
+        "Precision": precision_score(y_true, y_pred),
+        "Recall": recall_score(y_true, y_pred),
+        "F1": f1_score(y_true, y_pred),
+        "AUC": roc_auc_score(y_true, y_prob),
+        "KS": ks_score(y_true, y_prob),
+        "Brier": brier_score_loss(y_true, y_prob),
+        "LogLoss": log_loss(y_true, y_prob),
+        "Avg Precision Lift": average_precision_lift_score(y_true, y_prob),
+    }
